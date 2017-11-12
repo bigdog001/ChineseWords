3c253c9dd4ea6fa4994f2764da25f55968e3711d
unknown <jw362j@.ITServices.sbc.com>
2017-11-12 12:57:43 -0500

    initial projects

---
+++ src/test/java/com/mycomm/web/ChineseWords/TestMe.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomm.web.ChineseWords;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jw362j
+ */
+public class TestMe {
+
+    private static final Logger log = LoggerFactory.getLogger(TestMe.class);
+
+    @Before
+    public void setup() {
+        log.info("==========================junit setup====================================");
+    }
+
+    @Test
+    public void makeCodedWords() {
+ 
+    }
+
+       
+
+}
